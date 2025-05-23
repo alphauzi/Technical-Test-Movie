698e33b85cccce244035b66fc4bd6a2992fb6910
Yusron Alfauzi <alfauziyusron@gmail.com>
2025-05-23 16:26:55 +0700

    implement ui test

--- Technical Test MovieUITests/Technical_Test_MovieUITests.swift
+++ Technical Test MovieUITests/Technical_Test_MovieUITests.swift
@@ -7,37 +7,44 @@
 
 import XCTest
 
-//final class Technical_Test_MovieUITests: XCTestCase {
-//
-//    override func setUpWithError() throws {
-//        // Put setup code here. This method is called before the invocation of each test method in the class.
-//
-//        // In UI tests it is usually best to stop immediately when a failure occurs.
-//        continueAfterFailure = false
-//
-//        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-//    }
-//
-//    override func tearDownWithError() throws {
-//        // Put teardown code here. This method is called after the invocation of each test method in the class.
-//    }
-//
-//    @MainActor
-//    func testExample() throws {
-//        // UI tests must launch the application that they test.
-//        let app = XCUIApplication()
-//        app.launch()
-//
-//        // Use XCTAssert and related functions to verify your tests produce the correct results.
-//    }
-//
-//    @MainActor
-//    func testLaunchPerformance() throws {
-//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-//            // This measures how long it takes to launch your application.
-//            measure(metrics: [XCTApplicationLaunchMetric()]) {
-//                XCUIApplication().launch()
-//            }
-//        }
-//    }
-//}
+final class Technical_Test_MovieUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testExample() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let title = app.staticTexts["Movies"]
+        XCTAssertTrue(title.exists)
+        
+        let elementsQuery = app.otherElements
+        elementsQuery.element(boundBy: 6).tap()
+        
+        app/*@START_MENU_TOKEN@*/.staticTexts["Play trailer"]/*[[".buttons[\"Play trailer\"].staticTexts.firstMatch",".buttons.staticTexts[\"Play trailer\"]",".staticTexts[\"Play trailer\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        sleep(3)
+        app/*@START_MENU_TOKEN@*/.staticTexts["Review"]/*[[".cells.staticTexts[\"Review\"]",".staticTexts[\"Review\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeUp(velocity: .fast)
+        app.swipeUp(velocity: .fast)
+        app.swipeUp(velocity: .fast)
+        sleep(2)
+        app/*@START_MENU_TOKEN@*/.staticTexts["Back"]/*[[".buttons[\"Back\"].staticTexts.firstMatch",".buttons.staticTexts[\"Back\"]",".staticTexts[\"Back\"]"],[[[-1,2],[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        let element = elementsQuery.element(boundBy: 11)
+        element.swipeUp(velocity: .fast)
+        element.swipeUp(velocity: .fast)
+        element.swipeUp(velocity: .fast)
+    }
+
+    @MainActor
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
